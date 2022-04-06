db0d59ab25c17256120be2df320d160d647d805f
p-328 <69773020+p-328@users.noreply.github.com>
2022-04-06 10:00:01 -0400

    adding semicolons

--- hangman/main.go
+++ hangman/main.go
@@ -12,26 +12,26 @@ import (
 )
 func binarySearch(needle string, haystack []string) bool {
 	sort.Slice(haystack, func (i, j int) bool {
-		return haystack[i] < haystack[j]
-	})
-	low := 0
-	high := len(haystack) - 1
+		return haystack[i] < haystack[j];
+	});
+	low := 0;
+	high := len(haystack) - 1;
 
 	for low <= high{
-		median := (low + high) / 2
+		median := (low + high) / 2;
 
 		if haystack[median] < needle {
-			low = median + 1
+			low = median + 1;
 		}else{
-			high = median - 1
+			high = median - 1;
 		}
 	}
 
 	if low == len(haystack) || haystack[low] != needle {
-		return false
+		return false;
 	}
 
-	return true
+	return true;
 }
 func isEnglishWord(s string) bool {
 	dict, err := ioutil.ReadFile("words.txt");
@@ -44,7 +44,7 @@ func isEnglishWord(s string) bool {
 	if unicode.IsUpper(rune(s[0])) && unicode.IsLetter(rune(s[0])) {
 		for _, word := range wordList {
 			if unicode.IsLetter(rune(word[0])) {
-				unicode.ToUpper(rune(word[0]))
+				unicode.ToUpper(rune(word[0]));
 			}
 		}
 	}
@@ -55,13 +55,13 @@ func isEnglishWord(s string) bool {
 	return false;
 }
 func findLetterInString(letter string, s string) []int {
-	letter_bytes := []byte(letter)
-	string_bytes := []byte(s)
-	var instances []int
+	letter_bytes := []byte(letter);
+	string_bytes := []byte(s);
+	var instances []int;
 	letter_guess := string(letter_bytes[0])
 	for i := 0; i < len(string_bytes); i++ {
 		if letter_guess == string(string_bytes[i]) {
-			instances = append(instances, i+1)
+			instances = append(instances, i+1);
 		}
 	}
 	return instances;
@@ -74,18 +74,18 @@ func checkDuplicate(arr []string) bool {
 			}
 		} 
 	}
-	return false
+	return false;
 }
 
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano());
 	fmt.Println("Hangman");
 	for i := 0; i < 50; i++ {
 		fmt.Print("-");
 	}
-	var words []string
-	var wordAmt int
+	var words []string;
+	var wordAmt int;
 	attempts := 0;
 	attemptMax := 7;
 	fmt.Println();
@@ -95,20 +95,20 @@ func main() {
 		fmt.Println("That ain't a valid amount of words chief, so imma use the default.");
 		wordAmt = 20;
 	}
-	words = make([]string, wordAmt)
-	fmt.Println("Second we need to enter in words. So enter", wordAmt, "words, please")
+	words = make([]string, wordAmt);
+	fmt.Println("Second we need to enter in words. So enter", wordAmt, "words, please");
 	for i := 0; i < wordAmt; i++ {
 		fmt.Scan(&words[i])
-		phrase := strings.Fields(words[i])
+		phrase := strings.Fields(words[i]);
 		for _, word := range phrase {
 			if !isEnglishWord(word) {
-				fmt.Println("Imma stop you right there chief, one of your words isn't english.")
-				os.Exit(-1)
+				fmt.Println("Imma stop you right there chief, one of your words isn't english.");
+				os.Exit(-1);
 			}
 		}
 	}
 	if checkDuplicate(words) {
-		fmt.Println("You have a duplicate in your word set.")
+		fmt.Println("You have a duplicate in your word set.");
 		os.Exit(-1);
 	}
 	fmt.Println("Third, let's choose a mode. Enter easy for 10 attempts, hard for 5 attempts(default is medium for 7 attempts) ")
